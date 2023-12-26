b47454e450321d858367f8546ce982bee5e68a88
Vince Roy <vince@potionapps.com>
2023-12-26 17:38:30 -0600

    add expo updates

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {
-  IOS_LIBRARY_PATH, // Default iOS
-  IOS_DOCUMENT_PATH,
-  ANDROID_DATABASE_PATH, // Default Android
-  ANDROID_FILES_PATH,
-  ANDROID_EXTERNAL_FILES_PATH,
-  open,
-} from '@op-engineering/op-sqlcipher';
-
-const db = open({
-  name: 'myDb',
-  location: Platform.OS === 'ios' ? IOS_LIBRARY_PATH : ANDROID_DATABASE_PATH,
-});
-
 export default function App() {
   return (
     <View style={styles.container}>
